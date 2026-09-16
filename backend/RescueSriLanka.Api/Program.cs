3ca8dfc397617d684dfd98251fa86aae97f79cf6
IT24102382 <IT24102382@my.sliit.lk>
2026-09-16 16:11:27 +0530

    Add HelpRequest CRUD, urgency scoring, and travel advisory safety check

--- backend/RescueSriLanka.Api/Program.cs
+++ backend/RescueSriLanka.Api/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using RescueSriLanka.Api.Data;
+using RescueSriLanka.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add the DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IHelpRequestService, HelpRequestService>();
+builder.Services.AddScoped<ITravelAdvisoryService, TravelAdvisoryService>();
+builder.Services.AddControllers();
+
 var app = builder.Build();
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
